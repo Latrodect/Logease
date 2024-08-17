942886c35ea3643ac999420b9f52d6f7ad8152b1
Hasan Bahadır NURAL <h.bahadirnural@gmail.com>
2024-08-17 20:10:51 +0300

    Detail log decorators added.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='logease',
-    version='0.1.0',
+    version='0.2.0',
     description='A Python logging library designed to replace traditional logging mechanisms using decorators and modules.',
     long_description=long_description,
     long_description_content_type='text/markdown',

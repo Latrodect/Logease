cb76c2d64ee019620c7a0e5672dcdd8e5b211c23
Hasan Bahadır NURAL <h.bahadirnural@gmail.com>
2024-08-17 16:12:57 +0300

    Splunk, Elastic, API, SNMP, Email configs added

--- logless/modules/logger.py
+++ logless/modules/logger.py
@@ -1,4 +1,6 @@
 import logging
+from logless.handlers.request import SplunkHandler, ElasticSearchHandler, APIHandler, EmailHandler, SNMPHandler
+from logless.config.settings import LogConfig
 
 class CustomFormatter(logging.Formatter):
     grey = "\x1b[38;21m"
@@ -39,8 +41,76 @@ class Logger:
 
         self.console_handler = logging.StreamHandler()
         self.console_handler.setLevel(logging.DEBUG)
-        self.console_handler.setFormatter(CustomFormatter())  # Create an instance of CustomFormatter
+        self.console_handler.setFormatter(CustomFormatter())  
         self.logger.addHandler(self.console_handler)
+        self.setup()
+
+    def setup(self):
+        """
+        Configures logging handlers based on the current log configuration.
+
+        This method initializes the logging system based on the logging destination specified in the `LogConfig` instance.
+        It sets up appropriate handlers for different logging destinations such as Splunk, Elasticsearch, API endpoints,
+        local files, email, and SNMP traps. Each handler is configured with a custom formatter and added to the logger.
+
+        The method performs the following steps:
+        1. Retrieves the log destination and related configuration from `LogConfig`.
+        2. Based on the destination, it creates and configures the appropriate logging handler:
+            - **Splunk**: Configures a `SplunkHandler` if Splunk host and token are provided.
+            - **Elasticsearch**: Configures an `ElasticSearchHandler` if Elasticsearch host and index are provided.
+            - **API**: Configures an `APIHandler` if an API endpoint and key are provided.
+            - **Local File**: Configures a `FileHandler` if a local file path is specified.
+            - **Email**: Configures an `EmailHandler` if email recipients and SMTP server details are provided.
+            - **SNMP**: Configures an `SNMPHandler` if an SNMP trap receiver and community details are provided.
+        3. Adds each configured handler to the logger with a custom formatter for consistent log formatting.
+
+        This method assumes that `LogConfig` has already been instantiated and populated with necessary environment variables.
+        """
+        log_config = LogConfig() 
+
+        log_destination = log_config.get_log_destination()
+        
+        if log_destination == 'splunk' and log_config.splunk_host and log_config.splunk_token:
+            splunk_handler = SplunkHandler(log_config.splunk_host, log_config.splunk_token)
+            splunk_handler.setFormatter(CustomFormatter())
+            self.logger.addHandler(splunk_handler)
+
+        elif log_destination == 'elasticsearch' and log_config.elastic_host and log_config.elastic_index:
+            elastic_handler = ElasticSearchHandler(log_config.elastic_host, log_config.elastic_index)
+            elastic_handler.setFormatter(CustomFormatter())
+            self.logger.addHandler(elastic_handler)
+
+        elif log_destination == 'api' and log_config.api_endpoint and log_config.api_key:
+            api_handler = APIHandler(log_config.api_endpoint, log_config.api_key)
+            api_handler.setFormatter(CustomFormatter())
+            self.logger.addHandler(api_handler)
+
+        elif log_destination == 'local_file' and log_config.local_file_path:
+            file_handler = logging.FileHandler(log_config.local_file_path)
+            file_handler.setFormatter(CustomFormatter())
+            self.logger.addHandler(file_handler)
+
+        elif log_destination == 'email' and log_config.email_recipients:
+            email_handler = EmailHandler(
+                smtp_server=log_config.smtp_server,
+                smtp_port=log_config.smtp_port,
+                from_addr=log_config.email_from,
+                to_addrs=log_config.email_recipients.split(','),
+                subject='Log Notification',
+                username=log_config.smtp_username,
+                password=log_config.smtp_password
+            )
+            email_handler.setFormatter(CustomFormatter())
+            self.logger.addHandler(email_handler)
+
+        elif log_destination == 'snmp' and log_config.snmp_trap_receiver:
+            snmp_handler = SNMPHandler(
+                trap_receiver=log_config.snmp_trap_receiver,
+                community=log_config.snmp_community,
+                port=log_config.snmp_port
+            )
+            snmp_handler.setFormatter(CustomFormatter())
+            self.logger.addHandler(snmp_handler)
 
     def log(self, message, level="INFO"):
         """
